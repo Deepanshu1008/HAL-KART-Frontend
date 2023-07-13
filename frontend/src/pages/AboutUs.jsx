cef5241616d8cff7e007a2063c36e1a886e7746f
Deepanshu Gupta <89475845+Deepanshu1008@users.noreply.github.com>
2023-07-13 15:59:13 +0530

    Add files via upload

---
+++ frontend/src/pages/AboutUs.jsx
@@ -0,0 +1,15 @@
+
+
+const AboutUs = () => {
+  return (
+    <div className="text-color ms-5 me-5 mr-5 mt-3">
+      <b>
+      Hindustan Aeronautics Ltd (HAL) is an aerospace and defense company, owned by the government of India.
+       The company develops, designs, manufactures, and supplies aircraft,
+       helicopters, avionics, and communications equipment for military and civil markets.
+      </b>
+    </div>
+  );
+};
+
+export default AboutUs;
---
+++ frontend/src/pages/ContactUs.jsx
@@ -0,0 +1,12 @@
+
+const ContactUs = () => {
+  return (
+    <div className="text-color ms-5 me-5 mr-5 mt-3">
+      <b>
+      customer-service@hal-india.com
+      </b>
+    </div>
+  );
+};
+
+export default ContactUs;
